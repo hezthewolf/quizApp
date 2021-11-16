59864bc87a13b44dd9d508b8c340ce7b14b02c8e
Hez <mokuahesbon254@gmail.com>
2021-11-16 18:03:32 +0300

    MongoDB Connection and JWT initialization

--- app.js
+++ app.js
@@ -1,10 +1,29 @@
-const express = require('express')
-const app = express()
-const PORT = 5050
+const express = require("express");
+const axios = require("axios");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const app = express();
+const PORT = process.env.PORT || 9000
+const bodyParser = require('body-parser')
+let server;
 
-app.use(express.json())
+require("dotenv").config();
 
+// Middleware
+app.use(cors())
+app.use(bodyParser.urlencoded({ extended: true, limit: '20mb'}))
+app.use(bodyParser.json({limit: '20mb'}))
 
-app.listen(PORT, ()=>{
-    console.log(`Server is listening on port ${PORT}...`);
-})
+mongoose.connect(process.env.DB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}).then(() => {
+    console.log("Database connection established");
+}).catch(err => {
+    console.log("Error connecting to the mongoDB instance", err)
+});
+app.use(express.json());
+
+server = app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}...`);
+});
--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -1,104 +1,84 @@
-import React from 'react';
-import '../css/SignIn.css'
-import Separator from '../images/Separator.png'
+import React from "react";
+import "../css/SignIn.css";
+import Separator from "../images/Separator.png";
+import { useForm } from "../hooks/useForm";
 
+const SignIn = () => {
+  const [values, handleChange] = useForm({username: "", password: ""});
+  return (
+    <div>
+      <div className="nav-signin">
+        <div className="logo">Quivia</div>
+      </div>
 
+      <main>
+        <div className="title">Sign In</div>
 
+        <div className="card-sign">
+          <div>
+            <p className="login-para">
+              Enter your username and password to login
+            </p>
+          </div>
 
-class SignIn extends React.Component {
-    constructor() {
-        super()
-        this.state = {
-            username: "",
-            password: "",
-            remember: false
-        }
-        this.handleChange = this.handleChange.bind(this)
-    }
+          <div className="info">
+            <form>
+              <div className="username-sign">
+                <label for="username">Username</label>
+                <input
+                  id="username"
+                  name="username"
+                  type="text"
+                  value={values.username}
+                  required="required"
+                  onChange={handleChange}
+                />
+              </div>
 
-    handleChange(event) {
-        const {name, value, type, checked} = event.target
-        type === "checkbox" ? this.setState({[name]: checked}) : this.setState({[name]: value})
-    }
+              <div className="password">
+                <label for="password">Password</label>
+                <input
+                  id="password"
+                  name="password"
+                  type="password"
+                  value={values.password}
+                  required="required"
+                  onChange={handleChange}
+                />
+              </div>
 
-    render() {
-        return (
-            <div>
-                <div className="nav-signin">
-                    <div className="logo">
-                        Quivia
-                    </div>
-                </div>
+              <div className="remember">
+                <input
+                  type="checkbox"
+                  name="remember"
+                  id="remember"
+                  // checked={this.state.remember}
+                  // onChange={this.handleChange}
+                />
+                <label htmlFor="remember">Remember me</label>
+              </div>
 
-                <main>
-                    <div className="title">
-                        Sign In
-                    </div>
+              <div className="next">
+                <p>Log in</p>
+              </div>
 
+              <div className="forgot">
+                <a href={"./Forgot"}>Forgot password</a>
+              </div>
+            </form>
 
-                    <div className="card-sign">
-                    
-                        <div>
-                            <p className="login-para">Enter your username/email  and password to login</p>
-                        </div>
-                    
-                        <div className="info">
-
-                            <form>
-                                
-                                <div className="username-sign">
-                                    <label for="username">
-                                        Username
-                                    </label>
-                                    <input id="username" name="username" type="text" value={this.state.username} required="required" onChange={this.handleChange} />
-                                </div>
-
-
-                                <div className="password">
-                                    <label for="password">
-                                        Password
-                                    </label>
-                                    <input id="password" name="password" type="password" value={this.state.password} required="required" onChange={ this.handleChange }/>
-                                </div>
-
-                            
-                                <div className="remember">
-                                    <input type="checkbox" name="remember" id="remember" checked={this.state.remember} onChange={this.handleChange} />
-                                    <label htmlFor="remember">Remember me</label>
-                                </div>
-
-                                <div className="next">
-                                
-                                    <p>Log in</p>
-                                </div>
-
-                                <div className="forgot">
-                                    <a href={"./Forgot"}>
-                                        Forgot password
-                                    </a>
-                                </div>
-
-
-                            </form>
-                            
-
-                            <div className="separator-sign">
-                                <img id="separator" src={Separator} alt="Separator" />
-                            </div>
-
-                            <div className="google-sign"></div>
-                        </div>
-
-                    </div>
-
-                    <div className="copyright-sign">
-                        &copy; Quivia 2021
-                    </div>
-                </main>
+            <div className="separator-sign">
+              <img id="separator" src={Separator} alt="Separator" />
             </div>
-        )
-    }
-}
 
+            <div className="google-sign"></div>
+          </div>
+        </div>
+
+        <div className="copyright-sign">&copy; Quivia 2021</div>
+      </main>
+    </div>
+  );
+};
 
 export default SignIn;
